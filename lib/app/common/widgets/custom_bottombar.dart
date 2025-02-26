d55c8feb4f226c5d61599344e372c57619f0f508
athilaramdani <athilaramdani@gmail.com>
2025-02-27 00:02:19 +0700

    update bottom navigation ui

--- lib/app/common/widgets/custom_bottombar.dart
+++ lib/app/common/widgets/custom_bottombar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wastewise/app/common/theme/app_theme.dart';
 
-// Custom Bottom Navigation Bar
 class CustomBottomNavBar extends StatelessWidget {
   final int currentIndex;
 
@@ -11,50 +10,122 @@ class CustomBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      color: AppTheme.primaryGreen,
-      child: SizedBox(
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            // HOME
-            IconButton(
-              onPressed: () {
-                if (currentIndex != 0) {
-                  Get.offAllNamed('/home');
-                }
-              },
-              icon: Icon(
-                Icons.home,
-                color: currentIndex == 0 ? Colors.white : Colors.white54,
+    double width = MediaQuery.of(context).size.width;
+    double itemWidth = width / 3;
+    double bubbleSize = 60;
+
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          height: 70,
+          decoration: BoxDecoration(
+            color: AppTheme.primaryGreen,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 8,
+              )
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildNavItem(
+                  context,
+                  icon: Icons.home,
+                  index: 0,
+                  route: '/home',
+                  isSelected: currentIndex == 0),
+              _buildNavItem(
+                  context,
+                  icon: Icons.chat,
+                  index: 1,
+                  route: '/wisebot',
+                  isSelected: currentIndex == 1),
+              _buildNavItem(
+                  context,
+                  icon: Icons.person,
+                  index: 2,
+                  route: '/account',
+                  isSelected: currentIndex == 2),
+            ],
+          ),
+        ),
+        AnimatedPositioned(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOutBack,
+          left: (itemWidth * currentIndex) + (itemWidth / 2) - (bubbleSize / 2),
+          bottom: 20,
+          child: GestureDetector(
+            onTap: () {
+              if (currentIndex == 0) {
+                Get.offAllNamed('/home');
+              } else if (currentIndex == 1) {
+                Get.offAllNamed('/wisebot');
+              } else if (currentIndex == 2) {
+                Get.offAllNamed('/account');
+              }
+            },
+            child: Container(
+              width: bubbleSize,
+              height: bubbleSize,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 4,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder: (child, animation) =>
+                      ScaleTransition(scale: animation, child: child),
+                  child: Icon(
+                    currentIndex == 0
+                        ? Icons.home
+                        : currentIndex == 1
+                        ? Icons.chat
+                        : Icons.person,
+                    key: ValueKey<int>(currentIndex),
+                    size: 32,
+                    color: AppTheme.primaryGreen,
+                  ),
+                ),
               ),
             ),
-            // WISEBOT
-            IconButton(
-              onPressed: () {
-                if (currentIndex != 1) {
-                  Get.offAllNamed('/wisebot');
-                }
-              },
-              icon: Icon(
-                Icons.chat,
-                color: currentIndex == 1 ? Colors.white : Colors.white54,
-              ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNavItem(BuildContext context,
+      {required IconData icon,
+        required int index,
+        required String route,
+        required bool isSelected}) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          if (!isSelected) {
+            Get.offAllNamed(route);
+          }
+        },
+        child: SizedBox(
+          height: 70,
+          child: Center(
+            child: Icon(
+              icon,
+              size: 24,
+              color: isSelected ? Colors.transparent : Colors.white,
             ),
-            // ACCOUNT
-            IconButton(
-              onPressed: () {
-                if (currentIndex != 2) {
-                  Get.offAllNamed('/account');
-                }
-              },
-              icon: Icon(
-                Icons.person,
-                color: currentIndex == 2 ? Colors.white : Colors.white54,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
